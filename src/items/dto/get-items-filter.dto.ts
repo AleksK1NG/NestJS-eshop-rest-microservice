3b7dd191741996584f8e3ae893777fb4d73629e4
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-12-09 15:11:48 +0300

    add GetItemsFilterDto validation

--- src/items/dto/get-items-filter.dto.ts
+++ src/items/dto/get-items-filter.dto.ts
@@ -1,6 +1,12 @@
 import { ItemStatus } from '../item.model'
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator'
 
 export class GetItemsFilterDto {
+  @IsOptional()
+  @IsIn([ItemStatus.InStock, ItemStatus.OutOfStock, ItemStatus.Sale])
   status: ItemStatus
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string
 }
