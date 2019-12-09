dc0ca58aa9ac24b908340db03c238837f5db976b
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-12-09 14:19:14 +0300

    add create item handler

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Req } from '@nestjs/common'
+import { Body, Controller, Get, Post, Req } from '@nestjs/common'
 import { ItemsService } from './items.service'
 import { ISItem } from './item.model'
 
@@ -10,5 +10,12 @@ export class ItemsController {
   getAllItems(@Req() req): ISItem[] {
     console.log(req.url)
     return this.itemsService.getAllItems()
+  }
+
+  @Post()
+  createItem(@Body() body: ISItem) {
+    console.log(body)
+    const { description, title, price } = body
+    return this.itemsService.createItem(title, description, price)
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common'
-import { ISItem } from './item.model'
+import { ISItem, ItemStatus } from './item.model'
+import * as uuid from 'uuid/v1'
 
 @Injectable()
 export class ItemsService {
@@ -7,5 +8,19 @@ export class ItemsService {
 
   getAllItems(): ISItem[] {
     return this.items
+  }
+
+  createItem(title: string, description: string, price: number): ISItem {
+    const item: ISItem = {
+      id: uuid(),
+      title,
+      description,
+      price,
+      status: ItemStatus.InStock,
+    }
+
+    this.items.push(item)
+
+    return item
   }
 }
