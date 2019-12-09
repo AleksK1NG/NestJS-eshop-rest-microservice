d7c8ce09963b9fc6874e655999dee70215901d05
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-12-09 15:38:12 +0300

    add TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ItemsModule } from './items/items.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ItemsModule } from './items/items.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { typeOrmConfig } from './config/typeorm.config'
 
 @Module({
-  imports: [ItemsModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
