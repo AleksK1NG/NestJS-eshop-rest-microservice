f353f41d3f4670cdd422db7fbd1e05776860b5a5
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-12-09 18:50:09 +0300

    add Auth module and User Entity

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  username: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @Column()
+  salt: string
+}
