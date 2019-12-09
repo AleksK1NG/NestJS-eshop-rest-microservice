1644c972d59f2443c2ad23e796c758ab251dbc42
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-12-09 16:30:52 +0300

    add ItemRepository createItem handler

--- src/items/item.repository.ts
+++ src/items/item.repository.ts
@@ -1,7 +1,21 @@
 import { EntityRepository, Repository } from 'typeorm'
 import { Item } from './item.entity'
+import { CreateItemDto } from './dto/create-item.dto'
+import { ItemStatus } from './item-status.enum'
 
 @EntityRepository(Item)
 export class ItemRepository extends Repository<Item> {
+  async createItem(createItemDto: CreateItemDto): Promise<Item> {
+    const { title, description, price } = createItemDto
 
+    const item = new Item()
+    item.title = title
+    item.price = price
+    item.description = description
+    item.status = ItemStatus.Sale
+
+    await item.save()
+
+    return item
+  }
 }
