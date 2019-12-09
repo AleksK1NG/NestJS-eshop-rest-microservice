292db76be825cbcd4fe8f79398561e3ceb95129c
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-12-09 18:50:09 +0300

    add Auth module and User Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service'
 import { ItemsModule } from './items/items.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { typeOrmConfig } from './config/typeorm.config'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), ItemsModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), ItemsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
