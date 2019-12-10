e08f63c7b4e815c53636bb389de779e0e8069c5a
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-12-10 14:50:24 +0300

    add GetUser decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common'
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthCredentialsDto } from './dto/auth-credentials.dto'
 import { User } from './user.entity'
+import { AuthGuard } from '@nestjs/passport'
+import { GetUser } from './get-user.decorator'
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +17,12 @@ export class AuthController {
   @Post('/signin')
   signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto)
+  }
+
+  @Post('/me')
+  @UseGuards(AuthGuard())
+  loadUser(@GetUser() user: User): User {
+    console.log(user)
+    return user
   }
 }
