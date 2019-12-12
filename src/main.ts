2e3a935388bc052adfbd58ccd9acd6907d11dbb5
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-12-12 10:42:43 +0300

    add config with yml

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core'
+import * as config from 'config'
 import { AppModule } from './app.module'
 import { Logger } from '@nestjs/common'
 
 async function bootstrap() {
+  const serverConfig: any = config.get('server')
   const logger = new Logger('bootstrap')
-
   const app = await NestFactory.create(AppModule)
 
+  const port = process.env.PORT || serverConfig.port
   app.setGlobalPrefix('/api/v1')
-  const port = 3000
   await app.listen(port)
   logger.log(`Application listening on port ${port}`)
 }
